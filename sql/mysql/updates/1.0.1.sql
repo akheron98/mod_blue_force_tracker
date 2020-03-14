3277c43191ac8a17e0e3a05907bbd16989fcacb9
Gabriel Bourque <akheron98@gmail.com>
2020-03-14 10:57:47 -0400

    v1.0.1 v2

--- sql/mysql/updates/1.0.1.sql
+++ sql/mysql/updates/1.0.1.sql
@@ -1,2 +1,3 @@
+# Placeholder file for database changes for version 1.0.1
 UPDATE `#__blueforcetracker` SET `type`='' WHERE `type`='marker';
 UPDATE `#__blueforcetracker` SET `type`='' WHERE `type`='star';
