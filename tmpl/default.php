03c07ebf9270b730ad485ea5cb8b8ca95a9bde3a
Gabriel Bourque <akheron98@gmail.com>
2020-04-22 06:02:07 -0400

    Déplacement des balises scripts

--- tmpl/default.php
+++ tmpl/default.php
@@ -1,16 +1,10 @@
 <?php
 // No direct access
 defined('_JEXEC') or die; ?>
-<script src="/config-aws.js"></script>
-<script src="/modules/mod_blue_force_tracker/tmpl/js/customMapControl.js"></script>
-<script src="/modules/mod_blue_force_tracker/tmpl/js/mapbox-gl.js"></script>
 <link href="/modules/mod_blue_force_tracker/tmpl/css/mapbox-gl.css" rel="stylesheet"/>
 <link href="/modules/mod_blue_force_tracker/tmpl/css/blueforcetracker.css" rel="stylesheet"/>
 <link rel="stylesheet" href="/modules/mod_blue_force_tracker/tmpl/css/croppie.css" />
-<script src="/modules/mod_blue_force_tracker/tmpl/js/croppie.min.js"></script>
 <link rel="stylesheet" href="/modules/mod_blue_force_tracker/tmpl/css/bootstrap.min.css">
-<script src="/modules/mod_blue_force_tracker/tmpl/js/ui.js"></script>
-<script src="/modules/mod_blue_force_tracker/tmpl/js/blueforcetracker.js"></script>
 
 <div class="container-fluid" style="padding:0;">
     <div id="featuresStats" class="row" style="margin-left:0;"></div>
@@ -33,17 +27,22 @@ defined('_JEXEC') or die; ?>
     </div>
 </div>
 
+<script src="/modules/mod_blue_force_tracker/tmpl/js/customMapControl.js"></script>
+<script src="/modules/mod_blue_force_tracker/tmpl/js/mapbox-gl.js"></script>
+<script src="/modules/mod_blue_force_tracker/tmpl/js/croppie.min.js"></script>
+<script src="/modules/mod_blue_force_tracker/tmpl/js/ui.js"></script>
+<script src="/modules/mod_blue_force_tracker/tmpl/js/blueforcetracker.js"></script>
 <script>
-    const urlFeature = "https://m05rcnja4m.execute-api.us-east-2.amazonaws.com/prod/marker";
-    const urlPost = "<?php echo JURI::root() . "modules/mod_blue_force_tracker/tmpl/gateway.php"?>";
-    const joomlaUserId = "<?php echo $joomlaUserId;?>";
-    const isAdmin = "<?php echo $isAdmin;?>";
-    const connectedUser = joomlaUserId > 0;
     const map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/akheron/ck7rh7pw12b5c1is1hbldygdh',
         center: [-72.937107, 46.286173],
         zoom: 6.5
     });
+    const urlFeature = "https://m05rcnja4m.execute-api.us-east-2.amazonaws.com/prod/marker";
+    const urlPost = "<?php echo JURI::root() . "modules/mod_blue_force_tracker/tmpl/gateway.php"?>";
+    const joomlaUserId = "<?php echo $joomlaUserId;?>";
+    const isAdmin = "<?php echo $isAdmin;?>";
+    const connectedUser = joomlaUserId > 0;
     executeBlueForceTracker();
 </script>
